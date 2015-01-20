c5fd428a46c5a391260c179e9da6cb9ffe53c23c
lady3bean <annehawleybrett@gmail.com>
2015-01-20 16:34:01 -0500

    silent coding exercise for conway's game of life

--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,29 @@
 require_relative '../lib/conway'
+describe Grid do
+
+ subject(:grid) { Grid.new(y, x) }
+ let (:y) {2}
+ let (:x) {3}
+
+  describe '#initialize' do
+    it 'creates a new instance of a Grid' do
+      expect(grid).to be_a Grid
+    end
+  end
+  describe 'coordinates' do
+    it 'creates an array of arrays to form a grid with y, x coordinates starting from upper left corner' do
+      expect(grid.coordinates(y, x)).to eq[2][3] 
+    end
+  end
+end
+
+describe Cell
+
+  subject(:cell) { Cell.new }
+
+  describe '#initialize' do
+    it 'creates a new instance of a Cell in another Class' do
+      expect(cell).to be_a Cell
+    end
+  end
+end
